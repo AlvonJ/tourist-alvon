2b3c56467caa87cc932418cec0ebf6ea6205a7d5
Alvon Jovanus Chandra <alvonchan@gmail.com>
2023-01-25 12:14:27 +0700

    initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+const stripe = Stripe(
+   'pk_test_51MTkkGITe0epezbX1cz54enaOvcVqoilHx9GQy2vviTfnTz1I6uhTf8Q4LZURF01EHCrNng0nhrDGYYRDxmSbhn400fQqF1bx3'
+);
+
+export const bookTour = async tourId => {
+   try {
+      // 1) Get checkout session from API
+      const session = await axios(
+         `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+      );
+
+      // 2) Create checkout form + charge credit card
+      await stripe.redirectToCheckout({
+         sessionId: session.data.session.id,
+      });
+   } catch (err) {
+      console.log(err);
+      showAlert('error', err);
+   }
+};
---
+++ routes/tourRoutes.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
+
+const router = express.Router();
+
+// router.param('id', tourController.checkID);
+
+// POST /tour/234fasdsadsa/reviews
+
+// REDIRECT to reviewRouter
+router.use('/:tourId/reviews', reviewRouter);
+
+// Create a checkBody middleware
+// Check if body contains the name and price property
+
+// If not, send back 400 (bad request)
+// Add it to the post handler stack
+
+router
+   .route('/top-5-cheap')
+   .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+router
+   .route('/monthly-plan/:year')
+   .get(
+      authController.protect,
+      authController.restrictTo('admin', 'lead-guide', 'guide'),
+      tourController.getMonthlyPlan
+   );
+
+router
+   .route('/')
+   .get(tourController.getAllTours)
+   .post(
+      authController.protect,
+      authController.restrictTo('admin', 'lead-guide'),
+      tourController.createTour
+   );
+
+router
+   .route('/tours-within/:distance/center/:latlng/unit/:unit')
+   .get(tourController.getToursWithin);
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
+router
+   .route(`/:id`)
+   .get(tourController.getTour)
+   .patch(
+      authController.protect,
+      authController.restrictTo('admin', 'lead-guide'),
+      tourController.uploadTourImages,
+      tourController.resizeTourImages,
+      tourController.updateTour
+   )
+   .delete(
+      authController.protect,
+      authController.restrictTo('admin', 'lead-guide'),
+      tourController.deleteTour
+   );
+
+module.exports = router;
